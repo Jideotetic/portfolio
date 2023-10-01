b109b869cccd7330437ad90a9bdf49810dd1f3cd
Jideotetic <jideotetic@gmail.com>
2023-10-01 14:48:02 +0100

    Implemented the hamburger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nabla: "Nabla, cursive",
+      },
+      screens: {
+        mb: { max: "506px" },
+      },
+    },
   },
   plugins: [],
 };
